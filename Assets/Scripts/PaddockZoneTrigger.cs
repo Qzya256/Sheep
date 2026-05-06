03b6f9cb6573a5d73f454fe886a97fc66a9e1fcf
Andrew Kuznetcov <33774587+Qzya256@users.noreply.github.com>
2026-05-06 21:41:37 +0500

    Add GameOver logic

---
+++ Assets/Scripts/PaddockZoneTrigger.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PaddockZoneTrigger : MonoBehaviour
+{
+    [SerializeField] private LayerMask _crowdLayers;
+    [SerializeField] private int crowdCount;
+    [SerializeField] private int crowdMaxCount;
+    [SerializeField] private GameObject gameOverPanel;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PanicFleeFromTransform2D>()!= null)
+        {
+            crowdCount++;
+            CrowdChecker();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PanicFleeFromTransform2D>() != null)
+        {
+            crowdCount--;
+        }
+    }
+
+    private void CrowdChecker()
+    {
+        if (crowdCount >= crowdMaxCount)
+        {
+            gameOverPanel.SetActive(true);
+        }
+    }
+}
